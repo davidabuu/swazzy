d232d952b1987b0e5af2dd96b4e82094458e565a
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2022-12-20 15:21:06 +0100

    UI Better

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,23 @@
 import Header from "../src/components/Header";
 import UserWebLayout from "../src/components/UserWebLayout";
-import {Button} from 'antd'
+import { Button } from "antd";
+import Solution from "../src/components/Solution";
 
 export default function Home() {
   return (
-    <UserWebLayout webtitle='Solar Plant'>
+    <UserWebLayout webtitle="Solar Plant">
       <div className="container-landing">
-     <div className="img-overlay">
-     <Header/>
-     <div className="intro-container">
-      <h1>Monitoring of Solar Power Plant</h1>
-      <div className="btn">
-        <Button className="btn">LOGIN</Button>
+        <div className="img-overlay">
+          <Header />
+          <div className="intro-container">
+            <h1>Monitoring of Solar Power Plant</h1>
+            <div className="btn">
+              <Button className="btn">LOGIN</Button>
+            </div>
+          </div>
+        </div>
       </div>
-     </div>
-     </div>
-              </div>
+      <Solution />
     </UserWebLayout>
-  )
+  );
 }
