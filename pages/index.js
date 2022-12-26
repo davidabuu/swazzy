9f252c28787f6215328948cc4ce63ab96a25469c
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2022-12-26 13:58:34 +0100

    LOGIN FORM

--- pages/index.js
+++ pages/index.js
@@ -5,27 +5,30 @@ import Solution from "../src/components/Solution";
 import Footer from "../src/components/Footer";
 import Aos from "aos";
 import { useEffect } from "react";
-import 'aos/dist/aos.css';
+import "aos/dist/aos.css";
+import Link from "next/link";
 
 export default function Home() {
-useEffect(() => {
-  Aos.init()
-}, [])
+  useEffect(() => {
+    Aos.init();
+  }, []);
   return (
     <UserWebLayout webtitle="Solar Plant">
       <div className="container-landing">
         <div className="img-overlay">
           <Header />
-          <div data-aos='fade-right' className="intro-container">
+          <div data-aos="fade-right" className="intro-container">
             <h1>Monitoring of Solar Power Plant</h1>
             <div className="btn">
-              <Button className="btn">LOGIN</Button>
+              <Link href='/login'>
+                <Button className="btn">LOGIN</Button>
+              </Link>
             </div>
           </div>
         </div>
       </div>
       <Solution />
-      <Footer/>
+      <Footer />
     </UserWebLayout>
   );
 }
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,39 @@
-import React from 'react'
-
+import React from "react";
+import { Button, Input } from "antd";
+import UserWebLayout from "../src/components/UserWebLayout";
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <UserWebLayout webtitle='LOGIN'>
+      <div className="login">
+        <div className="form">
+          <form>
+            <h2>LOGIN</h2>
+            <div>
+              <div>
+                <label>Matric Number</label>
+                <br></br>
+                <Input type="text" placeholder="Enter FullName" required />
+              </div>
+            </div>
+            <div>
+              <div>
+                <label>Password</label>
+                <br></br>
+                <Input type="password" required />
+              </div>
+            </div>
 
-export default Login
+            <br></br>
+            <div>
+              <Button className="login-btn" htmlType="submit">
+                LOGIN
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </UserWebLayout>
+  );
+};
+
+export default Login;
